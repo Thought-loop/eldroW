997084453738b78b8cbb50a23a418d9a665aa579
Nick Steinhouse <79940765+Thought-loop@users.noreply.github.com>
2022-03-22 20:59:35 -0400

    Spring initializer initial configuration

---
+++ src/main/java/com/thoughtloop/eldroW/EldroWApplication.java
@@ -0,0 +1,13 @@
+package com.thoughtloop.eldroW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EldroWApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EldroWApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/thoughtloop/eldroW/EldroWApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thoughtloop.eldroW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EldroWApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

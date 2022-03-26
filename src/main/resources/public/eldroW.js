e37ee2ab779ebdf807741d52ac2ee5e5a4351328
Nick Steinhouse <nick.steinhouse@gmail.com>
2022-03-25 21:16:00 -0400

    fixed logic to alllow for new game after win

--- src/main/resources/public/eldroW.js
+++ src/main/resources/public/eldroW.js
@@ -24,6 +24,7 @@ document.addEventListener('DOMContentLoaded', () => {
     }));
 
     refreshButton.addEventListener('click', (event) => {
+        winner = false;
         resetBoard();
         getNewWord();
     });

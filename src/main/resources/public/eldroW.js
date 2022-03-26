e1093b9e6dc54a5252389ec649f904eef8a22cd5
Nick Steinhouse <nick.steinhouse@gmail.com>
2022-03-25 23:01:09 -0400

    fff

--- src/main/resources/public/eldroW.js
+++ src/main/resources/public/eldroW.js
@@ -184,9 +184,8 @@ function revealWord(){
             'Content-Type': 'application/json'
           },
         body: JSON.stringify({solutionId: gameSolutionID})
-        }).then(result => {result.json().then(data => displaySolution(data));
-            
-        }).catch(err => {
+        }).then(result => { displaySolution(data)}            
+        ).catch(err => {
             // if any error occured, then catch it here
             console.error(err);
         });
